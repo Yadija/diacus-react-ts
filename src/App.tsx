3c130445d7d72c4f925b0ffc9db780a76069518d
Yadija <ayuliadi01@gmail.com>
2023-09-25 11:05:04 +0800

    ⚒️build: change ui app

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,45 @@
-import './App.css';
-
-import { useState } from 'react';
-
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <main className='relative m-auto min-h-screen max-w-xl bg-[#F0F0F0]'>
+      <nav className='bg-[#213555] p-4 text-[#F0F0F0]'>
+        <h1>Diacus App</h1>
+      </nav>
+
+      <ul className='m-4 flex flex-col gap-2'>
+        <li>
+          <section className='flex w-full gap-2 bg-[#E5D283] px-2 py-4'>
+            <div className='relative h-12 w-12 rounded-full bg-[#213555]'>
+              <img
+                src='https://ui-avatars.com/api/?name=johndoe&background=random'
+                alt='johndoe'
+                sizes='100%'
+                className='rounded-full object-cover'
+              />
+            </div>
+            <article className='flex-1'>
+              <h2 className='text-sm'>@johndoe</h2>
+              <p className='text-2xl'>Content</p>
+            </article>
+          </section>
+        </li>
+        <li>
+          <section className='flex w-full gap-2 bg-[#E5D283] px-2 py-4'>
+            <div className='relative h-12 w-12 rounded-full bg-[#213555]'>
+              <img
+                src='https://ui-avatars.com/api/?name=janedoe&background=random'
+                alt='janedoe'
+                sizes='100%'
+                className='rounded-full object-cover'
+              />
+            </div>
+            <article className='flex-1'>
+              <h2 className='text-sm'>@janedoe</h2>
+              <p className='text-2xl'>Threads!!!</p>
+            </article>
+          </section>
+        </li>
+      </ul>
+    </main>
   );
 }
 
