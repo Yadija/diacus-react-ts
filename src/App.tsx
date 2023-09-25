43c3f430e9abd891375ce18e5cdc608aff1bf66d
Yadija <ayuliadi01@gmail.com>
2023-09-25 14:32:44 +0800

    ⚒️build: make ui register page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Routes>
     </main>
   );
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,17 +1,24 @@
 import { Link } from 'react-router-dom';
 
-function LoginPage() {
+function RegisterPage() {
   return (
     <div className='flex min-h-screen items-center justify-center'>
       <form
         className='flex w-[400px] flex-col gap-4 bg-[#213555] p-4'
         onSubmit={(event) => console.log(event)}
       >
-        <h1 className='text-center text-2xl font-bold text-[#F0F0F0]'>Login</h1>
+        <h1 className='text-center text-2xl font-bold text-[#F0F0F0]'>Register</h1>
         <input
           className='px-2 py-1'
           type='text'
           placeholder='username'
+          onChange={(event) => console.log(event)}
+          required
+        />
+        <input
+          className='px-2 py-1'
+          type='text'
+          placeholder='fullname'
           onChange={(event) => console.log(event)}
           required
         />
@@ -23,12 +30,12 @@ function LoginPage() {
           required
         />
         <button type='submit' className='mt-8 bg-[#E5D283] p-2 font-bold'>
-          Login
+          Register
         </button>
         <p className='text-center font-light text-[#F0F0F0]'>
-          {`Don't have an account? `}
-          <Link to='/register' className='text-[#E5D283]'>
-            Register
+          {`Already have an account? `}
+          <Link to='/login' className='text-[#E5D283]'>
+            Login
           </Link>
         </p>
       </form>
@@ -36,4 +43,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export default RegisterPage;
