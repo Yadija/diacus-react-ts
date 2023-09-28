707f475b9ea76189d5bc3e84d8bfc8aff81126b5
Yadija <ayuliadi01@gmail.com>
2023-09-28 19:36:39 +0800

    📦refactor: add interface in auth context

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,19 +1,24 @@
 import { createContext, Dispatch, ReactNode, SetStateAction, useState } from 'react';
 
+interface Auth {
+  accessToken?: string;
+  refreshToken?: string;
+}
+
 type AuthContextType = {
-  auth: object | null;
-  setAuth: Dispatch<SetStateAction<object | null>>;
+  auth: Auth | null;
+  setAuth: Dispatch<SetStateAction<Auth | null>>;
 };
 
 const iAuthContextState = {
-  auth: {},
+  auth: { accessToken: '', refreshToken: '' },
   setAuth: () => {},
 };
 
 export const AuthContext = createContext<AuthContextType>(iAuthContextState);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [auth, setAuth] = useState<object | null>(null);
+  const [auth, setAuth] = useState<Auth | null>(null);
 
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>
